85d48a90d162690e0a943fd65157e28c846cfbba
Aditya Garg <gargaditya08@live.com>
2026-07-29 13:18:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y lsb-release
 
-KERNEL_VERSION=7.1.3
-PKGREL=3
+KERNEL_VERSION=7.1.5
+PKGREL=2
 DISTRO=$(lsb_release -i | cut -d ":" -f 2 | xargs)
 CODENAME=$(lsb_release -c | cut -d ":" -f 2 | xargs)
 
